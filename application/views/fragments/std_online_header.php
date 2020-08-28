41e865d00f54b12fc5fbca48f8864e367bb74a9f
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-28 18:08:24 +0100

    repo updated from upstream

--- application/views/fragments/std_online_header.php
+++ application/views/fragments/std_online_header.php
@@ -33,7 +33,7 @@
             <a href="<?php echo base_url(); ?>studentProfile">
                 <button class="btn btn-lg btn-outline-light" data-target="#loginModal" data-toggle="modal">My Profile</button>
             </a>
-            <a href="#" class="ml-lg-5">
+            <a href="<?php echo base_url(); ?>tutors" class=" ml-lg-5">
                 <button class="btn btn-lg btn-danger">Instructors</button>
             </a>
         </div>
